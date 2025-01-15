0598c5ca54b455ef91b9e0669b74a41f704bec0c
Francis Legaspi <francis.legaspi@alc-tech.com.ph>
2025-01-15 19:04:04 +0800

    Applied page number solution

--- Sources/Streamer/Parser/EPUB/Services/EPUBPositionsService.swift
+++ Sources/Streamer/Parser/EPUB/Services/EPUBPositionsService.swift
@@ -21,6 +21,7 @@ public final class EPUBPositionsService: PositionsService {
             EPUBPositionsService(
                 readingOrder: context.manifest.readingOrder,
                 presentation: context.manifest.metadata.presentation,
+                pageList: context.manifest.subcollections["pageList"]?.first?.links,
                 fetcher: context.fetcher,
                 reflowableStrategy: reflowableStrategy
             )
@@ -49,14 +50,14 @@ public final class EPUBPositionsService: PositionsService {
             switch self {
             case let .originalLength(pageLength):
                 let length = resource.link.properties.encryption?.originalLength.map { UInt64($0) }
-                    ?? (try? resource.length.get())
-                    ?? 0
+                ?? (try? resource.length.get())
+                ?? 0
                 return max(1, Int(ceil(Double(length) / Double(pageLength))))
 
             case let .archiveEntryLength(pageLength):
                 let length = resource.link.properties.archive?.entryLength
-                    ?? (try? resource.length.get())
-                    ?? 0
+                ?? (try? resource.length.get())
+                ?? 0
                 return max(1, Int(ceil(Double(length) / Double(pageLength))))
             }
         }
@@ -64,13 +65,17 @@ public final class EPUBPositionsService: PositionsService {
 
     private let readingOrder: [Link]
     private let presentation: Presentation
+    private let pageList: [Link]
     private let fetcher: Fetcher
     private let reflowableStrategy: ReflowableStrategy
 
-    init(readingOrder: [Link], presentation: Presentation, fetcher: Fetcher, reflowableStrategy: ReflowableStrategy) {
+    init(readingOrder: [Link], presentation: Presentation,
+         pageList: [Link], fetcher: Fetcher,
+         reflowableStrategy: ReflowableStrategy) {
         self.readingOrder = readingOrder
         self.fetcher = fetcher
         self.presentation = presentation
+        self.pageList = pageList
         self.reflowableStrategy = reflowableStrategy
     }
 
@@ -118,18 +123,63 @@ public final class EPUBPositionsService: PositionsService {
     }
 
     private func makePositions(ofReflowableResource link: Link, from startPosition: Int) -> (Int, [Locator]) {
-        let resource = fetcher.get(link)
-        let positionCount = reflowableStrategy.positionCount(for: resource)
-        resource.close()
+        let href = link.href
+        var startIndexPosition = startPosition
 
-        let positions = (1 ... positionCount).map { position in
-            makeLocator(
-                for: link,
+        var positionRange = pageList
+            .filter { $0.href.hasPrefix(href)}
+            .compactMap { Int($0.title ?? "")}
+
+        let positionCount = pageList.filter { $0.href.hasPrefix(href) }.count
+
+        if !positionRange.isEmpty {
+            startIndexPosition = positionRange.first!
+        }
+
+        let skippedPages = findMissingNumbersUsingXor(positionRange)
+
+        return (0..<positionCount).compactMap { position in
+            let locatorPosition = startIndexPosition + position
+            if skippedPages.contains(locatorPosition) {
+                return nil
+            }
+            return createLocator(
+                href: href,
+                type: link.mediaType,
+                title: link.title,
                 progression: Double(position - 1) / Double(positionCount),
-                position: startPosition + position
+                position: locatorPosition
             )
         }
-        return (startPosition + positionCount, positions)
+    }
+
+    /**
+     Finds missing numbers in a list of increasing numbers.
+     According to the Content Team, it is possible to have skipped page numbers in an EPUB page-list.
+     (These are white/empty pages in a PDF that have been removed for EPUB.)
+     e.g., [1, 2, 3, 5, 8, 9] -> Pages 4 and 7 are missing.
+     This function identifies the missing numbers and skips them when creating `Publication.positions()`.
+     */
+    private func findMissingNumbersUsingXor(numbers: [Int]) -> [Int] {
+        guard !numbers.isEmpty else { return [] }
+
+        let min = numbers.min()!
+        let max = numbers.max()!
+
+        var xorRange = 0
+        for num in min...max {
+            xorRange ^= num
+        }
+
+        var xorList = 0
+        for num in numbers {
+            xorList ^= num
+        }
+
+        let fullRange = Set(min...max)
+        let actualNumbers = Set(numbers)
+
+        return fullRange.subtracting(actualNumbers).sorted()
     }
 
     private func makeLocator(for link: Link, progression: Double, position: Int) -> Locator {
