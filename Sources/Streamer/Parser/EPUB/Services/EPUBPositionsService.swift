0574c72e195faef327230631a28ec5f660e43ba0
Francis Legaspi <francis.legaspi@alc-tech.com.ph>
2025-03-12 18:54:11 +0800

    Applied workaround fix for page number solution

--- Sources/Streamer/Parser/EPUB/Services/EPUBPositionsService.swift
+++ Sources/Streamer/Parser/EPUB/Services/EPUBPositionsService.swift
@@ -21,6 +21,7 @@ public actor EPUBPositionsService: PositionsService {
             EPUBPositionsService(
                 readingOrder: context.manifest.readingOrder,
                 presentation: context.manifest.metadata.presentation,
+                pageList: context.manifest.subcollections["pageList"]?.first?.links ?? [],
                 container: context.container,
                 reflowableStrategy: reflowableStrategy
             )
@@ -60,17 +61,20 @@ public actor EPUBPositionsService: PositionsService {
 
     private let readingOrder: [Link]
     private let presentation: Presentation
+    private let pageList: [Link]
     private let container: Container
     private let reflowableStrategy: ReflowableStrategy
 
     init(
         readingOrder: [Link],
         presentation: Presentation,
+        pageList: [Link],
         container: Container,
         reflowableStrategy: ReflowableStrategy
     ) {
         self.readingOrder = readingOrder
         self.presentation = presentation
+        self.pageList = pageList
         self.container = container
         self.reflowableStrategy = reflowableStrategy
     }
@@ -128,19 +132,67 @@ public actor EPUBPositionsService: PositionsService {
     }
 
     private func makePositions(ofReflowableResource link: Link, from startPosition: Int) async -> (Int, [Locator]) {
-        guard let resource = container[link.url()] else {
-            return (startPosition, [])
-        }
-        let positionCount = await reflowableStrategy.positionCount(for: link, resource: resource)
+        let href = link.href
+        var startIndexPosition = startPosition
+        let positionRange = pageList
+            .filter { $0.href.hasPrefix(href)}
+            .compactMap { Int($0.title ?? "")}
+        let positionCount = pageList.filter { $0.href.hasPrefix(href) }.count
 
-        let positions = (1 ... positionCount).map { position in
-            makeLocator(
-                for: link,
-                progression: Double(position - 1) / Double(positionCount),
-                position: startPosition + position
+        if !positionRange.isEmpty {
+            startIndexPosition = positionRange.first!
+        }
+
+        let skippedPages = findMissingNumbersUsingXor(numbers: positionRange)
+
+        let positions = (0..<positionCount).compactMap { position -> Locator? in
+            let locatorPosition = startIndexPosition + position
+
+            if skippedPages.contains(locatorPosition) {
+                return nil
+            }
+
+            return Locator(
+                href: AnyURL(string: href)!,
+                mediaType: link.mediaType ?? .html,
+                title: link.title,
+                locations: .init(
+                    progression: Double(position - 1) / Double(positionCount),
+                    position: locatorPosition
+                )
             )
         }
-        return (startPosition + positionCount, positions)
+
+        return (startPosition, positions)
+    }
+
+    /**
+     Finds missing numbers in a list of increasing numbers.
+     According to the Content Team, it is possible to have skipped page numbers in an EPUB page-list.
+     (These are white/empty pages in a PDF that have been removed for EPUB.)
+     e.g., [1, 2, 3, 5, 8, 9] -> Pages 4 and 7 are missing.
+     This function identifies the missing numbers and skips them when creating `Publication.positions()`.
+     */
+    private func findMissingNumbersUsingXor(numbers: [Int]) -> [Int] {
+        guard !numbers.isEmpty else { return [] }
+
+        let min = numbers.min()!
+        let max = numbers.max()!
+
+        var xorRange = 0
+        for num in min...max {
+            xorRange ^= num
+        }
+
+        var xorList = 0
+        for num in numbers {
+            xorList ^= num
+        }
+
+        let fullRange = Set(min...max)
+        let actualNumbers = Set(numbers)
+
+        return fullRange.subtracting(actualNumbers).sorted()
     }
 
     private func makeLocator(for link: Link, progression: Double, position: Int) -> Locator {
