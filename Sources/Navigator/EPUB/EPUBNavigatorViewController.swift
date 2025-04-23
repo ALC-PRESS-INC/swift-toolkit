33ca4038d69e6a6128f8c82d87b59f07384ac522
Jonathan Andy Lim <jonathan.lim@alc-tech.com.ph>
2025-04-23 18:00:04 +0800

    TRY FIX page changes when going back to last page

--- Sources/Navigator/EPUB/EPUBNavigatorViewController.swift
+++ Sources/Navigator/EPUB/EPUBNavigatorViewController.swift
@@ -683,7 +683,7 @@ open class EPUBNavigatorViewController: UIViewController,
             positionList.count > 0
         {
             // Gets the current locator from the positionList, and fill its missing data.
-            let positionIndex = Int(ceil(progression * Double(positionList.count - 1)))
+            let positionIndex = Int(floor(progression * Double(positionList.count - 1)))
             return await positionList[positionIndex].copy(
                 title: tableOfContentsTitleByHref()[equivalent: href],
                 locations: { $0.progression = progression }
