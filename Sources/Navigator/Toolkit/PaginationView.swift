9ab5e42a9b33c86485cd2a35ca81bc7a8b33467b
Jonathan Andy Lim <jonathan.lim@alc-tech.com.ph>
2025-04-23 17:30:44 +0800

    FIX (2) When moving page backward, always start at the top of the page

--- Sources/Navigator/Toolkit/PaginationView.swift
+++ Sources/Navigator/Toolkit/PaginationView.swift
@@ -194,7 +194,8 @@ final class PaginationView: UIView, Loggable {
         // resource depending on the last index. This allows to navigate backward across resources,
         // starting from the end of each previous resource.
         let movingBackward = (currentIndex - 1 == index)
-        var location = location ?? (movingBackward ? .end : .start)
+//        var location = location ?? (movingBackward ? .end : .start)
+        var location = location ?? .start
         if let locator = pageIndexLocations[index] {
             location = .locator(locator)
             print("retained current location: \(location), at index \(index)")
